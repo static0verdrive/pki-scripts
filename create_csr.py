e57819455fcc340e60e7a7c0bc22c1c71c8aa69b
static <3944885+static0verdrive@users.noreply.github.com>
2023-05-08 14:43:20 -0400

    Reduce import lines

--- create_csr.py
+++ create_csr.py
@@ -2,8 +2,7 @@ import time
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 import ipaddress
 
