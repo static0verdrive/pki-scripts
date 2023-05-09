c54370e09b430803be87533f6c3275f664b4ca43
static <3944885+static0verdrive@users.noreply.github.com>
2023-05-08 20:53:39 -0400

    import AttributeOID for challenge password

--- create_csr.py
+++ create_csr.py
@@ -1,6 +1,6 @@
 import ipaddress
 from cryptography import x509
-from cryptography.x509.oid import NameOID
+from cryptography.x509.oid import NameOID, AttributeOID
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
